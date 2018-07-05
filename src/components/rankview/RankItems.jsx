526e9e7a495e8ba3955ebc756a76b0a5b9e2d536
handongming_win <980289875@qq.com>
2018-07-05 11:17:13 +0800

    'ReatItems'

---
+++ src/components/rankview/RankItems.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'antd';
+import './rankview.less';
+
+class RankItems extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            data:props.data
+        }
+    }
+    render() {
+        return (
+            123
+            // {/*<Time/>*/}
+            // {/*<Histograom/>*/}
+            // {/*<RankAxis/>*/}
+        );
+    }
+}
+
+export default RankItems;
--- src/components/rankview/rankview.jsx
+++ src/components/rankview/rankview.jsx
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
-import { Row, Col } from 'antd';
+import React, {Component} from 'react';
+import {Row, Col} from 'antd';
 import './rankview.less';
+import RankItems from './RankItems'
 
 class RankView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [1,2,3]
+        }
+    }
+
     render() {
-        return (
-            <div className="rank-wrapper">
-                <Row className="rank-wrapper-title">
-                    <Col span={8}></Col>
-                    <Col span={8}>
-                        Rank View
-                    </Col>
-                    <Col span={8}></Col>
-                </Row>
-                <div className="rank-wrapper-content"></div>
+        return <div className="rank-wrapper">
+            <Row className="rank-wrapper-title">
+                <Col span={8}/>
+                <Col span={8}>
+                    Rank View
+                </Col>
+                <Col span={8}/>
+            </Row>
+            <div className="rank-wrapper-content">
+                {this.state.data.map((i, d) => {
+                    return <RankItems data={d}/>
+                })}
             </div>
-        );
+        </div>;
     }
 }
 
